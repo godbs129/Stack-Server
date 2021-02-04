bf1ae8bc746d34bbc2c5501f699066113fb9786f
Junhaeyoon <junhaeyoon@gmail.com>
2021-02-05 00:00:15 +0900

    Add: 상/벌점 칼럼 추가

--- src/models/User.js
+++ src/models/User.js
@@ -16,10 +16,16 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    number: {
+    number: { //학번
       field: 'number',
       type: DataTypes.INTEGER,
       allowNull: true,
+    },
+    score: {
+      field: 'score',
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
     },
     salt: {
       type: DataTypes.STRING,
