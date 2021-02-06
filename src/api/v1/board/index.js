2891dc6aa5311eead4ba32b199924945e042e31e
Junhaeyoon <junhaeyoon@gmail.com>
2021-02-06 17:34:11 +0900

    Fix: 라우터 수정

--- src/api/v1/board/index.js
+++ src/api/v1/board/index.js
@@ -6,6 +6,6 @@ board.get('/', boardCtrl.readBoard);
 board.get('/my', auth, boardCtrl.readMyBoard);
 board.post('/', auth, boardCtrl.createBoard);
 board.put('/:idx', auth, boardCtrl.modifyBoard);
-board.delete('/', auth, boardCtrl.deleteBoard);
+board.delete('/:idx', auth, boardCtrl.deleteBoard);
 
 module.exports = board;
