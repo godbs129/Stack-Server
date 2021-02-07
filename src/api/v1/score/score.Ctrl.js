fd7494d89f780935f3cf5db65675f7136239b72d
Junhaeyoon <junhaeyoon@gmail.com>
2021-02-07 21:48:55 +0900

    Fix: 타입별 조회 추가 코드 수정

--- src/api/v1/score/score.Ctrl.js
+++ src/api/v1/score/score.Ctrl.js
@@ -7,7 +7,7 @@ exports.getMyScore = async (req, res) => {
   try {
     const decoded = await tokenLib.verifyToken(token);
 
-    const score = await models.User.getMyScore(decoded.id);
+    const score = await models.Score.getMyScore(decoded.id);
 
     res.status(200).json({
       code: 200,
@@ -16,6 +16,30 @@ exports.getMyScore = async (req, res) => {
         score,
       }
     })
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: '서버 오류',
+    })
+  }
+}
+
+exports.getMyScoreByType = async (req, res) => {
+  const token = req.headers['authorization'];
+  const type = req.query;
+
+  try {
+    const decoded = await tokenLib.verifyToken(token);
+
+    const score = await models.Score.getMyScoreByType(decoded.id, type);
+    console.log(score);
+    res.status(200).json({
+      code: 200,
+      message: '자신의 점수 조회 성공',
+      data: {
+        score,
+      }
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
