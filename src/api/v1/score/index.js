18684bcd962a9425bbb763ae953a871ae92775a0
Junhaeyoon <junhaeyoon@gmail.com>
2021-02-07 21:48:14 +0900

    Fix: 라우터 추가 및 수정

--- src/api/v1/score/index.js
+++ src/api/v1/score/index.js
@@ -2,6 +2,7 @@ const score = require('express').Router();
 const scoreCtrl = require('./score.Ctrl');
 const auth = require('../../../middleware/auth');
 
-score.get('/', auth, scoreCtrl.getMyScore);
+score.get('/', auth, scoreCtrl.getMyScoreByType);
+score.get('/all', auth, scoreCtrl.getMyScore);
 
 module.exports = score;
