3f8eb164f398b63d665143c983065768e85d6390
godbs129 <junhaeyoon@gmail.com>
2021-03-31 15:43:07 +0900

    Del: 필요없는 파일 삭제

--- src/api/v1/score/index.js
+++
@@ -1,8 +0,0 @@
-const score = require('express').Router();
-const scoreCtrl = require('./score.Ctrl');
-const auth = require('../../../middleware/auth');
-
-score.get('/', auth, scoreCtrl.getMyScoreByType);
-score.get('/all', auth, scoreCtrl.getMyScore);
-
-module.exports = score;
