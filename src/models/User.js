d8d38139505d6f62bdca6d74339bbdcb416761f0
godbs129 <junhaeyoon@gmail.com>
2021-03-31 15:42:08 +0900

    Add: 점수 컬럼 추가

--- src/models/User.js
+++ src/models/User.js
@@ -21,6 +21,23 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
+    bonusPoint: {
+      field: 'bonus_point',
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    minusPoint: {
+      field: 'minus_point',
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    type: {
+      field: 'type',
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     salt: {
       type: DataTypes.STRING,
     },
@@ -36,7 +53,17 @@ module.exports = (sequelize, DataTypes) => {
   `, {
     type: sequelize.QueryTypes.SELECT,
     raw: true,
-  })
+  });
+
+  User.getUserRankByType = (type) => sequelize.query(`
+  SELECT name score
+  FROM User
+  WHERE type = '${type}'
+  ORDER BY score DESC;
+  `, {
+    type: sequelize.QueryTypes.SELECT,
+    raw: true,
+  });
 
   return User;
 }
