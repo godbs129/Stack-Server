b6029ef00ab33a4d657ab610a1de093635bb3e38
godbs129 <junhaeyoon@gmail.com>
2021-03-31 15:42:28 +0900

    Fix: score -> point로 변경

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -2,6 +2,6 @@ const v1 = require('express').Router();
 
 v1.use('/auth', require('./auth'));
 v1.use('/board', require('./board'));
-v1.use('/score', require('./score'));
+v1.use('/point', require('./point'));
 
 module.exports = v1;
