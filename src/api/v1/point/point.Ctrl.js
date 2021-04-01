240bcdc380a6a7cf509716d438583ec1dfb60a83
godbs129 <junhaeyoon@gmail.com>
2021-04-01 11:59:00 +0900

    Refactor: 점수 랭킹 조회 기능 삭제 및 점수 부여 기능 추가

--- src/api/v1/point/point.Ctrl.js
+++ src/api/v1/point/point.Ctrl.js
@@ -34,7 +34,7 @@ exports.getMyScoreByType = async (req, res) => {
     const type = parseInt(query);
     const user = await models.User.getUserNameAndNumber(decoded.id);
     const score = await models.Score.getMyScoreByType(decoded.id, type);
-    console.log(score);
+
     res.status(200).json({
       code: 200,
       message: '자신의 점수 조회 성공',
@@ -51,19 +51,56 @@ exports.getMyScoreByType = async (req, res) => {
   };
 }
 
-exports.getUserRankingByType = async (req, res) => {
-  const query = req.query.type;
+exports.givePoint = async (req, res) => {
+  const token = req.headers['authorization'];
+  const { body } = req;
 
   try {
-    const type = parseInt(query);
-    const ranking = await models.Score.getUserRankingByType(type);
+    const decoded = await tokenLib.verifyToken(token);
+    const checkId = await models.User.findOne({
+      where: {
+        id: decoded.id,
+      }
+    });
+
+    if (checkId.type == 0) {
+      return res.status(401).json({
+        code: 401,
+        message: '권한이 없습니다',
+      });
+    };
+
+    const checkUser = await models.User.findOne({
+      where: {
+        id: body.userId,
+      }
+    });
+
+    if (!checkUser) {
+      return res.status(404).json({
+        code: 404,
+        message: '존재하지 않는 사용자 입니다!',
+      });
+    }
+
+
+    await models.Point.create({
+      type: body.type,
+      point: body.point,
+      reason: body.reason,
+      userId: body.userId,
+      giver: checkId.name,
+    });
+
+    if (body.type == 0) {
+      await models.User.updateUserBonusPoint(decoded.id, body.point);
+    } else if (body.type == 1) {
+      await models.User.updateUserMinusPoint(decoded.id);
+    }
 
     res.status(200).json({
       code: 200,
-      message: '점수 순위 조회 성공',
-      data: {
-        ranking,
-      }
+      message: '점수 부여 성공',
     });
   } catch (err) {
     console.log(err);
@@ -71,4 +108,4 @@ exports.getUserRankingByType = async (req, res) => {
       message: '서버 오류',
     });
   };
-}
+};
