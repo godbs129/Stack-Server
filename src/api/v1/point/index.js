29c71beb67d702a5297db7a6db19715f1bd458e4
godbs129 <junhaeyoon@gmail.com>
2021-04-01 11:58:06 +0900

    Del: 점수 랭킹 조회 라우터 삭제

--- src/api/v1/point/index.js
+++ src/api/v1/point/index.js
@@ -2,8 +2,8 @@ const point = require('express').Router();
 const pointCtrl = require('./point.Ctrl');
 const auth = require('../../../middleware/auth');
 
+point.post('/', auth, pointCtrl.givePoint);
 point.get('/', auth, pointCtrl.getMyScoreByType);
 point.get('/all', auth, pointCtrl.getMyScore);
-point.get('/rank', pointCtrl.getUserRankingByType);
 
 module.exports = point;
