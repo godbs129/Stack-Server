2f94a99a7d3558782ad4ba8f8b10aedaacd164f2
godbs129 <junhaeyoon@gmail.com>
2021-04-04 21:53:19 +0900

    Add: 이메일 인증 기능 추가

--- src/api/v1/auth/auth.Ctrl.js
+++ src/api/v1/auth/auth.Ctrl.js
@@ -1,6 +1,7 @@
 const models = require('../../../models');
 const token = require('../../../lib/token');
 const crypto = require('crypto');
+const nodemailer = require('nodemailer');
 
 exports.login = async (req, res) => {
   const { body } = req;
@@ -85,6 +86,7 @@ exports.register = async (req, res) => {
       id: body.id,
       pw: hashPw,
       name: body.name,
+      email: body.email,
       number: body.number,
       type: body.type,
       salt: salt,
@@ -139,4 +141,64 @@ exports.getUserRankMinusPoint = async (req, res) => {
       message: '서버 오류',
     });
   };
+}
+
+exports.resetUserPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await models.User.findOne({
+      where: { email: email },
+    });
+    if (user) {
+      const token = crypto.randomBytes(20).toString('hex');
+      const data = {
+        token,
+        userId: user.id,
+        ttl: 300,
+      };
+
+      models.Auth.create(data);
+
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.gmail,
+          pass: process.env.mail_pw,
+        },
+      });
+
+      const mailOptions = {
+        from: process.env.gmail,
+        to: email,
+        subject: '[Stack] 비밀번호 초기화',
+        text: 'This is the authentication code to find the password!', // 내용
+        html:
+          `<p>비밀번호 초기화를 위해서는 아래의 URL을 클릭하여 주세요.<p>` +
+          `<a href='http://localhost:2030/resetaccount/${token}'>비밀번호 새로 입력하기</a>`, // TODO : 링크는 웹 라우터에 따라 변경하기
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('이메일 전송')
+        }
+      });
+      return res.status(200).json({
+        code: 200,
+        message: '비밀번호 초기화 메일 전송 성공!',
+      })
+    } else {
+      return res.status(401).json({
+        code: 401,
+        message: '이메일을 확인하세요.'
+      })
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: '서버 오류',
+    });
+  }
 }
